9e31cff84c8cd56b160bb5f7be837361cc1b39d9
Junyoung Yang <y@jun0.dev>
2025-09-25 22:03:46 +0900

    feat:백엔드 프로젝트 초기화 (#1)

---
+++ backend/src/test/java/com/donzo/naitssu/NaItssuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donzo.naitssu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaItssuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
