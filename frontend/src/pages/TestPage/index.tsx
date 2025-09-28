328f573ec23a83fad6c82fc118b9f0d9a1897412
chahyunlee <chahyun023@gmail.com>
2025-09-28 17:36:18 +0900

    feat: 테스트 페이지 라우팅 및 chatbot 추가

---
+++ frontend/src/pages/TestPage/index.tsx
@@ -0,0 +1,11 @@
+import { Chatbot } from "@/components/chatbot";
+
+export const TestPage = () => {
+  return (
+    <div className="min-h-screen bg-gray-50 p-8">
+      <Chatbot />
+    </div>
+  );
+};
+
+export default TestPage;
